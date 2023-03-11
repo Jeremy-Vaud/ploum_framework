0933837391ed5fb516e3440b6812ea6ca807e333
Jeremy-Vaud <100855862+Jeremy-Vaud@users.noreply.github.com>
2023-03-11 22:16:08 +0100

    Add help to manage.php

--- manage.php
+++ manage.php
@@ -7,6 +7,13 @@ require 'vendor/autoload.php';
 if(isset($argv[1])) {
     switch ($argv[1]){
 
+        case 'help':
+            echo "\n";
+            echo "migrate : Génére ou modifie les tables de la BDD\n";
+            echo "create-admin-pannel : Génére un fichier JSON pour la construction du panneau d'administration\n";
+            echo "\n";
+            break;
+
         case 'migrate':
             $migration = new App\Migration;
             $migration->migrate();
