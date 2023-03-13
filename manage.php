bbf2759fbdd2d96213958804a71eb915f212b315
Jeremy-Vaud <100855862+Jeremy-Vaud@users.noreply.github.com>
2023-03-13 22:31:00 +0100

    Création de compte admin en ligne de commande

--- manage.php
+++ manage.php
@@ -4,14 +4,14 @@
  */
 require 'vendor/autoload.php';
 
-if(isset($argv[1])) {
-    switch ($argv[1]){
-
+echo "\n";
+if (isset($argv[1])) {
+    switch ($argv[1]) {
         case 'help':
-            echo "\n";
+            echo "Liste des commandes:\n";
             echo "migrate : Génére ou modifie les tables de la BDD\n";
+            echo "create-admin-user 'email' 'password': Créer un compte admin\n";
             echo "create-admin-pannel : Génére un fichier JSON pour la construction du panneau d'administration\n";
-            echo "\n";
             break;
 
         case 'migrate':
@@ -19,15 +19,43 @@ if(isset($argv[1])) {
             $migration->migrate();
             break;
 
+        case "create-admin-user":
+            if ($argc !== 4) {
+                echo "Paramètres de la commande invalide\n";
+                echo "Commande pour créer un admin : php manage.php create-admin-user 'email' 'password'\n";
+            } else {
+                $user = new App\User;
+                $check = $user->checkData(["email" => $argv[2], "password" => $argv[3]]);
+                if ($check !== []) {
+                    foreach ($check as $key => $error) {
+                        echo "Erreur $key : $error\n";
+                    }
+                } else {
+                    $user->set("nom", "admin");
+                    $user->set("prenom", "admin");
+                    $user->set("email", $argv[2]);
+                    $user->set("password", $argv[3]);
+                    $user->set("admin", 1);
+                    if ($user->insert()) {
+                        echo "Nouvel admin crée\n\n";
+                    } else {
+                        echo "Une erreur est survenue";
+                    }
+                }
+            }
+            break;
+
         case 'create-admin-pannel':
             $adminPannel = new App\AdminPannel;
             $adminPannel->generate();
             break;
-        
-        default:
-            echo "Commande inconnue";
 
+        default:
+            echo "Commande inconnue\n";
+            echo "Utilisez 'php manage.php help' pour plus d'informations\n";
     }
 } else {
-    echo "Aucune commande";
+    echo "Aucune commande\n";
+    echo "Utilisez 'php manage.php help' pour plus d'informations\n";
 }
+echo "\n";
