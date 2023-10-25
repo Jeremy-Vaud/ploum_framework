edc8788abbc207822d7791237880fc04347c684d
Jeremy-Vaud <100855862+Jeremy-Vaud@users.noreply.github.com>
2023-10-25 13:07:04 +0200

    maj update image

--- adminSrc/components/FormImage.js
+++ adminSrc/components/FormImage.js
@@ -4,17 +4,30 @@ import { v4 as uuidv4 } from 'uuid'
 export default function FormImage(props) {
     const id = uuidv4()
     const [hasFile, setHasFile] = useState(false)
+    const [hasTmp, setHasTmp] = useState(false)
     const [src, setSrc] = useState("");
+    const [tmpSrc, setTmpSrc] = useState("");
 
     useEffect(() => {
         if (props.value !== null && props.value !== "") {
-            setSrc("../"+props.value)
+            setSrc("../" + props.value)
             setHasFile(true)
         }
     }, [])
 
     function change() {
         setHasFile(false)
+    }
+
+    function handleChange(e) {
+        props.handleChange(e)
+        setHasTmp(true)
+        setTmpSrc(URL.createObjectURL(e.target.files[0]))
+    }
+
+    function tmpChange() {
+        setHasTmp(false)
+        document.getElementById(id).value = ""
     }
 
     return (
@@ -26,13 +39,20 @@ export default function FormImage(props) {
             </label>
             {hasFile ?
                 <>
-                    <img src={src} className="mx-auto"/>
+                    <img src={src} className="mx-auto" />
                     <div className="text-center">
                         <span onClick={change} className="btn-update mt-2">Supprimer l'image</span>
                     </div>
                 </>
                 :
-                <input type="file" name={props.name} id={id} onChange={props.handleChange} accept="image/*" className="file:btn-add" />}
+                <>
+                    <img src={tmpSrc} className={hasTmp ? "mx-auto" : "hidden"} />
+                    <div className={hasTmp ? "text-center" : "hidden"}>
+                        <span onClick={tmpChange} className="btn-update mt-2">Supprimer l'image</span>
+                    </div>
+                    <input type="file" name={props.name} id={id} onChange={handleChange} accept="image/*" className={hasTmp ? "hidden" : "file:btn-add"} />
+                </>
+            }
         </div>
     )
 }
